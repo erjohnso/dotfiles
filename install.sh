e960cd697f61ed0c871a39fcecb107d8ab6435e4
Eric Johnson <erjohnso@google.com>
2013-09-13 12:37:20 +0000

    added installer

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This repo is just an easy transport mechanism for installing my base rc
+# scripts.  On a new machine, install git, clone this this dotfiles repo
+# and run 'dotfiles/install.sh' to just copy all files to $HOME.
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for dotfile in .bash_aliases .bash_profile .gitconfig .irssi .tmux.conf .vimrc
+do
+    if [ -f $HOME/$dotfile ]; then
+        echo "noclobber: $HOME/$dotfile already exists, so skipping!"
+    else
+        cp -R $DIR/$dotfile $HOME
+    fi
+done
