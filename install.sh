cc7469b77701de9a0749ee18c673e9b945164b72
Eric Johnson <erjohnso@google.com>
2014-11-13 17:54:46 +0000

    support yellow prompt for docker containers

--- install.sh
+++ install.sh
@@ -15,5 +15,10 @@ do
     fi
 done
 
+if [ "$1" = "-docker" ]; then
+    # If passing in '-docker', set prompt to yellow
+    echo 'export PS1="\n\[\033[01;36m\]\w\n\[\033[01;33m\]\u@\h\$\[\033[00m\] "' >> $HOME/.bash_profile
+fi
+
 # clean up after running
 rm -rf $DIR
